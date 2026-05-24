dbcc38dd29eee5d312d75f4769682aacb7bb66bd
Ch1ngkeMate <19239371547@163.com>
2026-05-24 17:31:05 +0800

    fix: 修复 WXSS 路径 和 config.js 域名

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -1,5 +1,5 @@
 /** 后端 API 根地址（须为 HTTPS，且已在微信公众平台配置 request 合法域名） */
-const API_BASE = "https://你的备案域名";
+const API_BASE = "https://shaoxiaoli.top";
 
 function getApiBase() {
   return API_BASE.replace(/\/$/, "");
